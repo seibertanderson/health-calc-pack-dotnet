99f55a9991ddb6e63ff6c703c231cc82c566e5b2
Anderson Seibert <andersonseibert0@gmail.com>
2022-11-23 18:46:37 -0300

    - Final Code

--- src/health-calc-dotnet/health-calc-console-dotnet/Program.cs
+++ src/health-calc-dotnet/health-calc-console-dotnet/Program.cs
@@ -4,28 +4,44 @@ using health_calc_pack_dotnet.Enums;
 Console.WriteLine("Entre com a sua Altura e Peso para Calcular seu IMC");
 Console.WriteLine("----------------------------------------------------");
 
-Console.Write("Altura: ");
+Console.Write("Qual sua Altura: ");
 double Height = double.Parse(Console.ReadLine());
 
-Console.Write("Peso: ");
+Console.Write("Qual seu Peso Peso: ");
 double Weight = double.Parse(Console.ReadLine());
 
-Console.Write("Sexo (F ou M): ");
+Console.WriteLine();
+Console.WriteLine("F - Feminino");
+Console.WriteLine("M - Masculino");
+Console.Write("Qual seu Sexo: ");
 string Sex = Console.ReadLine();
 SexoEnum sexoEnum = Sex.ToUpper() == "F" ? SexoEnum.Feminino : SexoEnum.Masculino;
 
-//Console.Write("Nível de Atividade Fisica (F ou M): ");
-//string NivelAtividade = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Sedentario = 0");
+Console.WriteLine("Moderadamente Ativo = 1");
+Console.WriteLine("Bastante Ativo = 2");
+Console.WriteLine("Extremamente Ativo = 3");
+Console.Write("Qual seu Nível de Atividade Fisica: ");
+NivelAtividadeFisicaEnum NivelAtividade = (NivelAtividadeFisicaEnum)int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+Console.WriteLine("Aumentar Peso (Bulking) = 0");
+Console.WriteLine("Perder Gordura (Cutting) = 1");
+Console.WriteLine("Manter meu Peso (Maintenance) = 2");
+Console.Write("Qual seu Objetivo Físico: ");
+ObjetivoFisicoEnum ObjetivoFisico = (ObjetivoFisicoEnum)int.Parse(Console.ReadLine());
 
 var objImc = new IMC();
 var objMacronutriente = new Macronutriente();
 
-var Result = objImc.Calc(Height, Weight);
-var Category = objImc.GetIMCCategory(Result);
+var CalculoIMC = objImc.Calc(Height, Weight);
+var CategoriaIMC = objImc.GetIMCCategory(CalculoIMC);
+var ResultMacroNutriente = objMacronutriente.Calc(sexoEnum, Height, Weight, NivelAtividade, ObjetivoFisico);
 
-Console.WriteLine($"Seu IMC é: {Result} - {Category}");
-
-var ResultMacroNutriente = objMacronutriente.Calc(sexoEnum, Height, Weight, NivelAtividadeFisicaEnum.ModeradamenteAtivo, ObjetivoFisicoEnum.Maintenance);
+Console.WriteLine();
+Console.WriteLine("##########  RESULTADO  ##########");
+Console.WriteLine($"Seu IMC é: {CalculoIMC} - {CategoriaIMC}");
 Console.WriteLine($"Seu consumo de Macronutrientes deve ser:");
 Console.WriteLine($"Proteinas: {ResultMacroNutriente.Proteinas} Gramas ");
 Console.WriteLine($"Gorduras: {ResultMacroNutriente.Gorduras} Gramas ");
